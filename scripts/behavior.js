5ba525fd69cf363b6b45a007049e2ec807a5196a
Tomás Folgado Louro Dias Costa <ist190783@tecnico.ulisboa.pt>
2021-10-30 19:31:53 +0100

    bars alligned and shieeeet mate

--- scripts/behavior.js
+++ scripts/behavior.js
@@ -1,15 +1,15 @@
 var table_11_incidents_src = "data/table11-Incidents.csv";
 var table_11_offenses_src = "data/table11-Offenses.csv";
-var table_1_offenses_src = "data/table1-Offenses.csv"
+var table_1_offenses_src = "data/table1-Offenses.csv";
 var map = "data/countries-110m.json";
 
 var topology;
 
-Promise.all([d3.json(map), d3.csv(table_11_offenses_src), d3.csv(table_1_offenses_src)]).then(function ([
-  map,
-  table_11_offenses,
-  table_1_offenses
-]) {
+Promise.all([
+  d3.json(map),
+  d3.csv(table_11_offenses_src),
+  d3.csv(table_1_offenses_src),
+]).then(function ([map, table_11_offenses, table_1_offenses]) {
   topology = map;
   //console.log(table_11_offenses);
   //console.log(map);
@@ -39,13 +39,13 @@ function createLineChart(table_11) {
   const height = 150;
   margin = { top: 10, right: 15, bottom: 20, left: 35 };
 
- // console.log(table_11[1]);
+  // console.log(table_11[1]);
   data = trableReformatYearsSingleBias(table_11[1]);
 
   line = d3
     .line()
     .defined(function (d) {
-   //   console.log(d);
+      //   console.log(d);
       return d.year;
     })
     .x((d) => x(d.year))
@@ -140,7 +140,6 @@ function createLineChart(table_11) {
 
 /***********************************************************************************/
 
-
 /*function tableGetX(data) {
     //console.log(data);
     var out;
@@ -169,154 +168,151 @@ function createLineChart(table_11) {
 }*/
 
 function tableGetInfo(data, year) {
-    //console.log(data);
-    var out = [];
-    var out_value;
-    var bias_type;
-    for (const [key, value] of Object.entries(data)) {
-        //console.log(value);
-        //for (const [kkey, vvalue] of Object.entries(value)) {
-        //  console.log(kkey);
-        //    console.log(vvalue);
-        //}~
-        out_value = -1;
-        for (const [kkey, vvalue] of Object.entries(value)) {
-            if (kkey == year) {
-                out_value = vvalue;
-            }
-            bias_type = vvalue; 
-        }
-        out.push({line: bias_type, value: out_value});    
-        //break;    
+  //console.log(data);
+  var out = [];
+  var out_value;
+  var bias_type;
+  for (const [key, value] of Object.entries(data)) {
+    //console.log(value);
+    //for (const [kkey, vvalue] of Object.entries(value)) {
+    //  console.log(kkey);
+    //    console.log(vvalue);
+    //}~
+    out_value = -1;
+    for (const [kkey, vvalue] of Object.entries(value)) {
+      if (kkey == year) {
+        out_value = vvalue;
+      }
+      bias_type = vvalue;
     }
-    return out;
+    out.push({ line: bias_type, value: out_value });
+    //break;
+  }
+  console.log(out);
+  return out;
 }
 
 function createBarChart(data, update, year) {
+  width = 600;
+  height = 350;
 
-    width = 600;
-    height = 300;
+  margin = { top: 20, right: 20, bottom: 20, left: 40 };
 
-    margin = {top:20, right:20, bottom:20, left:40};
-
-    //console.log(data);
-   /* x_values = []
+  //console.log(data);
+  /* x_values = []
     x_values.push(tableGetX(data[2]));
     x_values.push(tableGetX(data[8]));
     x_values.push(tableGetX(data[16]));
     x_values.push(tableGetX(data[22]));
     x_values.push(tableGetX(data[25]));*/
-    //console.log(x_values);
+  //console.log(x_values);
 
-    x = d3
-        .scaleBand()
-        .domain(["Race", "Religion", "Sexual Orientation", "Ethnicity/National Origin", "Disability"])
-        .range([margin.left, width - margin.right]);
-  
-    y = d3
-        .scaleLinear()
-        .domain([0, 10000])
-        .range([height - margin.bottom, margin.top]);
-        //.padding(0.5);
+  x = d3
+    .scaleBand()
+    .domain([
+      "Race",
+      "Religion",
+      "Sexual Orientation",
+      "Ethnicity/National Origin",
+      "Disability",
+    ])
+    .range([margin.left, width - margin.right]);
 
-    function xAxis(g) {
-        g.attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(x));
-    }
+  y = d3
+    .scaleLinear()
+    .domain([0, 10000])
+    .range([height - margin.bottom, margin.top]);
+  //.padding(0.5);
 
-    function yAxis(g) {
-        g.attr("transform", `translate(${margin.left}, 0)`).call(
-            d3
-                .axisLeft(y)
-                //.tickFormat((i) => {
-                  //  if (data[i].oscar_year % 3 == 0) return data[i].oscar_year;
-                //})
-                .tickSizeOuter(0)
+  function xAxis(g) {
+    g.attr("transform", `translate(0, ${height - margin.bottom})`).call(
+      d3.axisBottom(x)
+    );
+  }
 
-        );
-    }
-    
+  function yAxis(g) {
+    g.attr("transform", `translate(${margin.left}, 0)`).call(
+      d3
+        .axisLeft(y)
+        //.tickFormat((i) => {
+        //  if (data[i].oscar_year % 3 == 0) return data[i].oscar_year;
+        //})
+        .tickSizeOuter(0)
+    );
+  }
 
-    if (!update) {
-        d3
-        .select("div#barChart")
-        .append("svg")
-        .append("g")
-        .attr("class", "bars")
-        .attr("fill", "steelblue");
-    }
+  if (!update) {
+    d3.select("div#barChart")
+      .append("svg")
+      .append("g")
+      .attr("class", "bars")
+      .attr("fill", "steelblue");
+  }
 
-    const svg = d3
+  const svg = d3
     .select("div#barChart")
     .select("svg")
     .attr("width", width)
     .attr("height", height);
 
-    var dict_lines = tableGetInfo(data, year);
-    console.log(dict_lines);
+  var dict_lines = tableGetInfo(data, year);
+  //console.log(dict_lines);
 
-   /* for(i = 0; i < dict_lines.length; i++) {
-        if (dict_lines[i].line == "Race:") {
-           // console.log("encontrei");
-            //console.log(dict_lines[i].information.length);
-            for (j = 0; j < dict_lines[i].information.length; j++) {
-                if (dict_lines[i].information[j].year == 2019)
-                    console.log("Value of 2019: " + dict_lines[i].information[j].value);
-            }
-        }
-    }*/
-
-
-
-    new_data = dict_lines.filter(function(d) {
-        if (d.line == "Race:" || d.line == "Religion:" ||d.line == "Sexual Orientation:" ||d.line == "Ethnicity/National Origin:" ||d.line == "Disability:" ) {
-            return d;
-        }
-    });
+  new_data = dict_lines.filter(function (d) {
+    if (
+      d.line == "Race:" ||
+      d.line == "Religion:" ||
+      d.line == "Sexual Orientation:" ||
+      d.line == "Ethnicity/National Origin:" ||
+      d.line == "Disability:"
+    ) {
+      return d;
+    }
+  });
   // bars
   svg
     .select("g.bars")
     .selectAll("rect")
-    .data(new_data, function(d) {
-        return d.value;
+    .data(new_data, function (d) {
+      return d.value;
     })
     .join(
-        (enter) => {
-            return enter
-            .append("rect")
-            .attr("x", (d) => x(d.line))
-            .attr("y", (d) => y(d.value))
-            .attr("width", x.bandwidth())
-            .attr("height", (d) => height - y(d.value))
-            //.on("mouseover", handleMouseHover)
-            //.on("mouseleave", handleMouseLeave)
-            //.on("click", handleClick);
-        },
-        (update) => {
-            update
-            .attr("x", (d) => x(d.line))
-            .attr("y", (d) => y(d.value))
-            .attr("width", x.bandwidth())
-            .attr("height", (d) => height - y(d.value))
-            /*.attr("x", x(0))
+      (enter) => {
+        return enter
+          .append("rect")
+          .attr("x", function (d) {
+            console.log(d);
+            return x(d.line.replace(":", ""));
+          })
+          .attr("y", (d) => y(d.value))
+          .attr("width", x.bandwidth())
+          .attr("height", (d) => height - margin.bottom - y(d.value));
+        //.on("mouseover", handleMouseHover)
+        //.on("mouseleave", handleMouseLeave)
+        //.on("click", handleClick);
+      },
+      (update) => {
+        update
+          .attr("x", (d) => x(d.line))
+          .attr("y", (d) => y(d.value))
+          .attr("width", x.bandwidth())
+          .attr("height", (d) => height - y(d.value));
+        /*.attr("x", x(0))
             .attr("y", (d, i) => y(i))
             .attr("width", (d) => x(d.rating) - x(0))
             .attr("height", x.bandwidth());*/
-        },
-        (exit) => {
-            exit.remove();
-        }
-
+      },
+      (exit) => {
+        exit.remove();
+      }
     );
-        
-    if (!update) {
-        svg.append("g").attr("class", "xAxis");
-        svg.append("g").attr("class", "yAxis")
-    }
 
-    
-    svg.select("g.xAxis").call(xAxis);
+  if (!update) {
+    svg.append("g").attr("class", "xAxis");
+    svg.append("g").attr("class", "yAxis");
+  }
 
-    svg.select("g.yAxis").call(yAxis);
+  svg.select("g.xAxis").call(xAxis);
 
+  svg.select("g.yAxis").call(yAxis);
 }
